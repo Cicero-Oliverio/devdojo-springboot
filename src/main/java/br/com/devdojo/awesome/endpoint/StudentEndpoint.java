fa34cdbd9dd796f0ef8bbeb2188e80ce8f8de066
Cicero Olivério <cicero_oliverio@hotmail.com>
2018-11-12 00:28:59 -0200

    initial commit

---
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -0,0 +1,29 @@
+package br.com.devdojo.awesome.endpoint;
+
+import br.com.devdojo.awesome.model.model.Student;
+import br.com.devdojo.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndpoint {
+
+    @Autowired
+    private DateUtil dateUtil;
+    @RequestMapping(method = RequestMethod.GET,path = "/list")
+    public List<Student> listAll(){
+
+        dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now());
+        return asList(new Student("Deku"),new Student("Todoroki"));
+    }
+
+
+}
